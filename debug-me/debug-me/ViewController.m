cbbf572c58b3601abfe0016f15bd21e7a84287f7
David Wagner <mnem@noiseandheat.com>
2016-04-05 23:42:27 +0100

    Basic web requests.

--- debug-me/debug-me/ViewController.m
+++ debug-me/debug-me/ViewController.m
@@ -8,20 +8,56 @@
 
 #import "ViewController.h"
 
+static NSString * const JSONEndpoint = @"http://localhost:12345/index.json";
+static NSString * const PNGEndpoint = @"http://localhost:12345/index.png";
+
 @interface ViewController ()
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *buttons;
 
 @end
+
+NS_ASSUME_NONNULL_BEGIN
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)disableButtons {
+    for (UIButton *button in self.buttons) {
+        button.enabled = NO;
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)enableButtons {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        for (UIButton *button in self.buttons) {
+            button.enabled = YES;
+        }
+    });
+}
+
+- (void)fetchContentFrom:(NSString *)endpoint completionHandler:(void (^)(NSData * __nullable data, NSURLResponse * __nullable response, NSError * __nullable error))completionHandler {
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    NSURLSessionDataTask *task = [session dataTaskWithURL:[NSURL URLWithString:endpoint]
+                                        completionHandler:completionHandler];
+    [task resume];
+}
+
+- (IBAction)handleFetchJSONTapped:(UIButton *)sender {
+    [self disableButtons];
+    [self fetchContentFrom:JSONEndpoint completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"Response: %@", response);
+        [self enableButtons];
+    }];
+}
+
+- (IBAction)handleFetchPNGTapped:(UIButton *)sender {
+    [self disableButtons];
+    [self fetchContentFrom:PNGEndpoint completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"Response: %@", response);
+        [self enableButtons];
+    }];
 }
 
 @end
+
+NS_ASSUME_NONNULL_END
